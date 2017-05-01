88032892e443130a793a0eb362563cfb4e5cf850
prashant.pandey <anupam@successivesoftwares.com>
2017-05-01 11:54:34 +0530

    Intial commit with git_ignore and add AppLocalization

---
+++ AppLocalization/AppLocalization/AppLocalization/App.cs
@@ -0,0 +1,35 @@
+﻿using AppLocalization.Resx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace AppLocalization
+{
+	public class App : Application
+	{
+        public App ()
+		{
+            // The root page of your application
+            MainPage = new NavigationPage(new ChooseLanguagePage());
+                   
+		}
+
+        protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ AppLocalization/AppLocalization/AppLocalization/ChooseLanguagePage.cs
@@ -0,0 +1,46 @@
+﻿using AppLocalization.Resx;
+using System;
+using System.Collections.Generic;
+using AppLocalization.Interface;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppLocalization
+{
+    public class ChooseLanguagePage : ContentPage
+    {
+        public ChooseLanguagePage()
+        {
+            var btnEnglish = new Button() { Text = "English", BackgroundColor = Color.Aqua, VerticalOptions = LayoutOptions.CenterAndExpand };
+            btnEnglish.Clicked += BtnEnglish_Clicked;
+            var btnChinese = new Button() { Text = "Chinese", BackgroundColor = Color.Aqua, VerticalOptions = LayoutOptions.CenterAndExpand };
+            btnChinese.Clicked += BtnChinese_Clicked;
+            Content = new StackLayout
+            {
+                Padding = new Thickness(20),
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Select Language",
+                            FontAttributes = FontAttributes.Bold
+                        },
+                        btnEnglish,
+                        btnChinese,
+                    }
+            };
+        }
+
+        private void BtnChinese_Clicked(object sender, EventArgs e)
+        {
+            DependencyService.Get<ILocalise>().SetLocale(new System.Globalization.CultureInfo("zh-Hant"));
+            Navigation.PushAsync(new AppLocalisationXaml());
+        }
+
+        private void BtnEnglish_Clicked(object sender, EventArgs e)
+        {
+            DependencyService.Get<ILocalise>().SetLocale(new System.Globalization.CultureInfo("en-US"));
+            Navigation.PushAsync(new AppLocalisationXaml());
+        }
+    }
+}
